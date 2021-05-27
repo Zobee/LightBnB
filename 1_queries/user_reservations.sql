9743fac776e3886d9a1f579963114980a58b3b3d
Zobee <zohebboga@hotmail.com>
2021-05-27 00:51:24 +0000

    Added project boilerplate

--- 1_queries/user_reservations.sql
+++ 1_queries/user_reservations.sql
@@ -1,8 +1,9 @@
-SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
-FROM properties
-JOIN property_reviews ON properties.id = property_id
-JOIN reservations ON reservation_id = reservations.id
-WHERE property_reviews.guest_id = 1
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
 GROUP BY properties.id, reservations.id
-HAVING end_date < now()::date
-ORDER BY start_date;
+ORDER BY reservations.start_date
+LIMIT 10;
